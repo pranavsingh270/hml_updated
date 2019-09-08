b1aa1c001ee566f4c4e877eaed02f3e604d0582e
unknown <pranavsingh270@gmail.com>
2019-09-08 17:31:46 +0530

    edited routing for about and contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import {IndustryComponent} from './industry/industry.component';
 import { ServicesComponent } from './services/services.component';
 import { TeamComponent } from './team/team.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'industry',component:IndustryComponent},
   {path:'services',component:ServicesComponent},
   {path:'team',component:TeamComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'about',component:AboutComponent},
   {path:'**',redirectTo:'/home',pathMatch:'full'}
 ];
 
