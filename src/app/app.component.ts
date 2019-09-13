3d2dbf6ea4f1e6de597989249b4eb80dfa2d4c22
unknown <pranavsingh270@gmail.com>
2019-09-13 22:58:18 +0530

    nav bar correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isShown:boolean = false;
   title = 'hml-app';
+
+  navbarOpen = false;
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 }
